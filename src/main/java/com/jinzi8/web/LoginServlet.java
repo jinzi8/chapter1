ffaf467e00429471b67d8e48c3ac2cf14d6e8dff
jinzi8 <jzzswb@163.com>
2019-03-14 14:50:25 +0800

    修改bug

--- src/main/java/com/jinzi8/web/LoginServlet.java
+++ src/main/java/com/jinzi8/web/LoginServlet.java
@@ -7,14 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "/login.do")
-public class Servlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("msg","你好");
         System.out.println("执行了doGet方法");
-    request.getRequestDispatcher("/login,jsp").forward(request,response);
+    request.getRequestDispatcher("/login.jsp").forward(request,response);
     }
 }
